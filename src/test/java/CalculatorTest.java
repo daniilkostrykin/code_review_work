4187f6d164439bce1bc392023fb6a14f3c86e928
Daniil Kostrykin <daniilkostrykin1@gmail.com>
2025-11-24 00:19:47 +0300

    Changes from Kostrykin

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,31 +1,37 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.add(2, 3));
+        assertEquals(0, calculator.add(-2, 2));
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.dif(5, 3));
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.div(6, 3));
+        assertThrows(IllegalArgumentException.class, () -> calculator.div(5, 0));
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        Calculator calculator = new Calculator();
+        assertEquals(6, calculator.times(2, 3));
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        Calculator calculator = new Calculator();
+        assertEquals(16, calculator.solver());
     }
 }
